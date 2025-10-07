10d8967666880ad090c74fb4f04620843b11c41d
HG570 <rafaelpinheirovgp@gmail.com>
2025-10-07 00:17:40 -0300

    fix: process error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}
-  }
+  },
+  optimizeDeps: {
+    include: ['process'],
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        inject({
+          process: 'process/browser',
+        }),
+      ],
+    },
+  },
 })
